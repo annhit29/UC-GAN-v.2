fd0b5ff758561ad4ad52e8760ceaeb781c20cc4f
annhit29 <ann20010929@gmail.com>
2023-12-29 01:31:43 +0100

    txt2bmp.py works correctly for 26 alphabets (a to z)

--- txt2bmp.py
+++ txt2bmp.py
@@ -31,18 +31,35 @@ convert txt to bmp files with colors
 
 #     return matrix
 
+
+'''
+change to 32 values +1/31 each time because of Lorenz cipher that can encode to /394+8
+Each character corresponds to a value of `arr` in the solver_rotor: a-> 0, b-> 0.03225806451612903, etc., z->0.8064516129032258, /-> 0.8387096774193548 ..., 8-> 1.0
+'''
+arr_when_with_lorenz = [0.0, 0.03225806451612903, 0.06451612903225806, 0.0967741935483871, 0.12903225806451613, 0.16129032258064516, 
+    0.1935483870967742, 0.22580645161290322, 0.25806451612903225, 0.29032258064516125, 0.3225806451612903, 0.3548387096774194, 
+    0.3870967741935484, 0.4193548387096774, 0.45161290322580644, 0.4838709677419355, 0.5161290322580645, 0.5483870967741935, 
+    0.5806451612903225, 0.6129032258064516, 0.6451612903225806, 0.6774193548387096, 0.7096774193548387, 0.7419354838709677, 
+    0.7741935483870968, 0.8064516129032258, 0.8387096774193548, 0.8709677419354839, 0.9032258064516129, 0.9354838709677419, 
+    0.967741935483871, 1.0]
+
+'''
+Each alphabet corresponds to a value of `arr` in the solver_rotor: 0-> a, 0.0392->b, etc., 1.0-> z
+'''
 arr = [0.,     0.0392, 0.0784, 0.1176, 0.1569, 0.1961,
         0.2353, 0.2745, 0.3137, 0.3529, 0.3922, 0.4314,
         0.4706, 0.5098, 0.5490, 0.5882, 0.6275, 0.6667,
         0.7059, 0.7451, 0.7843, 0.8235, 0.8627, 0.9020,
         0.9412, 1.0000]
+
+
 '''
 convert txt to bmp files with only black, white, and grey colors
 '''
 def txt2bw(text):
     formed_string = text.encode('utf-8')
 
-    pixel_len = len(formed_string)
+    # pixel_len = len(formed_string)
     width = 100
     height = 1
 
@@ -50,50 +67,29 @@ def txt2bw(text):
     while len(formed_string) < (width * height):  # Padding
         formed_string += b'\x00'
 
-    # This line initializes a 2D matrix (list of lists) of size width by height with all values set to 0.
-    matrix = [[0 for _ in range(width)] for _ in range(height)]
+    # Convert text data into an image
+    image = Image.frombytes('L', (width, height), formed_string)
+    # https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python
     
-    for y in range(height):
-        for x in range(width):
-            char_value = formed_string[x]
-            index = int((char_value / 255) * (len(arr) - 1))  # Mapping the char value to the range of arr
-            grey_value = int(arr[index] * 255)  # Scale the arr value to the range of grayscale (0-255)
-            matrix[y][x] = (grey_value, grey_value, grey_value)
-            # bit_value = 0
-            # if x < pixel_len:
-            #     # for i in range(8):
-            #     #     bit_value |= (char_value & (1 << i)) >> i
-                
-            #     # # Make the black color more prominent
-            #     # matrix[y][x] = (0, 0, 0) if bit_value else (255, 255, 255)
+    # Convert the image to grayscale
+    grayscale_image = image.convert('L')
+    
+    # print(grayscale_image.size) #(100, 1)
 
-            #     # Count the number of set bits in the character byte
-            #     bit_count = bin(char_value).count('1')
-            #     # Scale the bit count to a range of grey values (0-255)
-            #     grey_value = int((bit_count / 8) * 255)
-            #     matrix[y][x] = (grey_value, grey_value, grey_value)
-    
+    # Extract grayscale pixel data
+    grayscale_pixels = list(grayscale_image.getdata())
+
+    # Reshape pixel data into a matrix
+    matrix = [grayscale_pixels[i * width:(i + 1) * width] for i in range(height)]
+
     return matrix
 
-'''
-creating a textual representation of the RGB (or in the context of the black and white conversion, the grayscale values) matrix
-'''
-def matrix2rep(matrix):
-    height = len(matrix)
-    width = len(matrix[0])
-    representation = "Width={}, height={}\n".format(width, height)
-    for y in range(height):
-        for x in range(width):
-            r, g, b = matrix[y][x]
-            representation += "rgb({0: >3}, {1: >3}, {2: >3})  ".format(r, g, b)
-        representation += "\n"
-    return representation
 
 
 def matrix2bmp(matrix, filename):
     height = len(matrix)
     width = len(matrix[0])
-    im = Image.new("RGB", (width, height))
+    im = Image.new('L', (width, height))
     pixels = im.load()
 
     for y in range(height):
@@ -133,21 +129,16 @@ def main():
 
     if text is not False:
         matrix = txt2bw(text)#txt2rgb(text)
+        #todo: `matrix2bmp`'s stuff already done in txt2bw, no need to convert from Image back to matrix THEN bacl to Image by calling matrix2bmp
         if args.bitmap is not None:
             if matrix2bmp(matrix, args.bitmap):
                 print("Saved as bitmap in: {}".format(args.bitmap))
             else:
                 print("Failed to save as bitmap...")
-        elif args.text is not None:
-            try:
-                with open(args.text, "w") as f:
-                    f.write(matrix2rep(matrix))
-                print("Saved as text in: {}".format(args.text))
-            except:
-                print("Failed to save as text...")
-        else:
-            print(matrix2rep(matrix))
-
 
 if __name__ == "__main__":
     main()
+
+
+
+#inspired src: https://github.com/Hamz-a/txt2bmp/tree/master?tab=readme-ov-file
